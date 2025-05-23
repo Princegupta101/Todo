eafd60642a4aead49437781db2532e2ffcd218c8
Princegupta101 <pgupta28349@gmail.com>
2025-05-23 21:37:15 +0530

    upadted hosting  setup

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://todo-xqtp.onrender.com/api';
 
 export const getTodos = () => axios.get(`${API_BASE_URL}/todos`);
 
